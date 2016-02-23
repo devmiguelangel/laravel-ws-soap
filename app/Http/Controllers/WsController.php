e58550fd04d6cc044302d1ed6922cb22c1c14508
Miguel Angel <djmiguelarango@gmail.com>
2016-02-23 19:56:23 +0000

    Ws Test, Client

---
+++ app/Http/Controllers/WsController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Artisaninweb\SoapWrapper\Facades\SoapWrapper;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class WsController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function test()
+    {
+        SoapWrapper::add(function($service) {
+            $service
+                ->name('currency')
+                ->wsdl('http://currencyconverter.kowabunga.net/converter.asmx?WSDL')
+                ->trace(true)                                                   // Optional: (parameter: true/false)
+                // ->header()                                                      // Optional: (parameters: $namespace,$name,$data,$mustunderstand,$actor)
+                // ->customHeader($customHeader)                                   // Optional: (parameters: $customerHeader) Use this to add a custom SoapHeader or extended class
+                // ->cookie()                                                      // Optional: (parameters: $name,$value)
+                // ->location()                                                    // Optional: (parameter: $location)
+                // ->certificate()                                                 // Optional: (parameter: $certLocation)
+                ->cache(WSDL_CACHE_NONE)                                        // Optional: Set the WSDL cache
+                ->options(['login' => 'username', 'password' => 'password']);   // Optional: Set some extra options
+        });
+
+        $data = [
+            'CurrencyFrom' => 'USD',
+            'CurrencyTo'   => 'EUR',
+            'RateDate'     => '2014-06-05',
+            'Amount'       => '1000'
+        ];
+
+        // Using the added service
+        SoapWrapper::service('currency', function ($service) use ($data) {
+            var_dump($service->getFunctions());
+            var_dump($service->call('GetConversionAmount', [$data])->GetConversionAmountResult);
+        });
+    }
+
+    public function wsClient()
+    {
+        SoapWrapper::add(function($service) {
+            $service
+                ->name('currency')
+                ->wsdl('http://10.16.11.16:8077/WS/WebServiceSudamericana.asmx?WSDL')
+                ->trace(true)
+                ->cache(WSDL_CACHE_NONE)
+                ->options(['wPwd' => 't874j563bk580fghu']);
+        });
+
+        $data = [
+            'wDocId' => '6929262',
+        ];
+
+        // Using the added service
+        SoapWrapper::service('currency', function ($service) use ($data) {
+            var_dump($service->getFunctions());
+            var_dump($service->call('su_PersonaGetByDocId', [$data])->GetConversionAmountResult);
+        });
+    }
+
+}
